a16e22bc7a6ba3e9b8eaab077cce813e67d3b758
tnnidm <910207973@qq.com>
2021-09-01 00:06:45 +0800

    update hashmap solution for twoSum

--- 001_TwoSum/twoSum.go
+++ 001_TwoSum/twoSum.go
@@ -1,13 +1,25 @@
 package twoSum
 
 // Solution 1: brute-force search
+// func twoSum(nums []int, target int) []int {
+// 	for i, num1 := range nums {
+// 		for j, num2 := range nums {
+// 			if (i != j) && (num1+num2 == target) {
+// 				return []int{i, j}
+// 			}
+// 		}
+// 	}
+// 	return nil
+// }
+
+// Solution 2: map
 func twoSum(nums []int, target int) []int {
-	for i, num1 := range nums {
-		for j, num2 := range nums {
-			if (i != j) && (num1+num2 == target) {
-				return []int{i, j}
-			}
+	m1 := make(map[int]int)
+	for i, num := range nums {
+		if j, ok := m1[target-num]; ok {
+			return []int{j, i}
 		}
+		m1[num] = i
 	}
 	return nil
 }
