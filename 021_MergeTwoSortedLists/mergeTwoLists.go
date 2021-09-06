2e0b20c823539ef74477101a379d6d305f46f641
tnnidm <910207973@qq.com>
2021-09-06 13:13:19 +0800

    complete 021_MergeTwoSortedLists

---
+++ 021_MergeTwoSortedLists/mergeTwoLists.go
@@ -0,0 +1,29 @@
+package mergeTwoLists
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 执行用时：0 ms, 在所有 Go 提交中击败了100.00%的用户
+// 内存消耗：2.5 MB, 在所有 Go 提交中击败了73.72%的用户
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	head := &ListNode{}
+	temp := head
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			temp.Next = l1
+			l1 = l1.Next
+		} else {
+			temp.Next = l2
+			l2 = l2.Next
+		}
+		temp = temp.Next
+	}
+	if l1 != nil {
+		temp.Next = l1
+	} else {
+		temp.Next = l2
+	}
+	return head.Next
+}
