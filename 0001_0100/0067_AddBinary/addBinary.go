40b25065c0f9820c608bc32854e139d479d8ac91
tnnidm <910207973@qq.com>
2021-09-14 14:47:20 +0800

    0067_AddBinary

---
+++ 0001_0100/0067_AddBinary/addBinary.go
@@ -0,0 +1,30 @@
+package addBinary
+
+import "strings"
+
+func addBinary(a string, b string) string {
+	lenA := len(a)
+	lenB := len(b)
+	if lenB > lenA {
+		a, b = b, a             //swap
+		lenA, lenB = lenB, lenA //swap2
+	}
+	b = strings.Repeat("0", lenA-lenB) + b
+	lenB = lenA
+	carry := byte(0)
+	ret := make([]byte, lenB+1)
+
+	for i := lenB - 1; i >= 0; i-- {
+		numA := a[i] - '0'
+		numB := b[i] - '0'
+		sum := numA + numB + carry
+		ret[i+1] = sum&1 + '0'
+		carry = sum >> 1
+	}
+	if carry == 0 {
+		ret = ret[1:]
+	} else {
+		ret[0] = carry + '0'
+	}
+	return string(ret)
+}
