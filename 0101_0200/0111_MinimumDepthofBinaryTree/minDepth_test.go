7634a993510f51d82e3a8094672e3569224783b1
tnnidm <910207973@qq.com>
2021-10-04 11:50:38 +0800

    complete 0111_MinimumDepthofBinaryTree

---
+++ 0101_0200/0111_MinimumDepthofBinaryTree/minDepth_test.go
@@ -0,0 +1,69 @@
+package minDepth
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	// 测试用例
+	cases := []struct {
+		name   string
+		input  *TreeNode
+		expect int
+	}{
+		{"case 1", &TreeNode{Val: 1}, 1},
+		{
+			"case 2",
+			&TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val: 2,
+					Left: &TreeNode{
+						Val: 3,
+					},
+					Right: &TreeNode{
+						Val: 4,
+					},
+				},
+				Right: &TreeNode{
+					Val: 2,
+					Left: &TreeNode{
+						Val: 4,
+					},
+					Right: &TreeNode{
+						Val: 3,
+					},
+				},
+			},
+			3,
+		},
+		{
+			"case 3",
+			&TreeNode{
+				Val: 1,
+				Left: &TreeNode{
+					Val: 2,
+					Left: &TreeNode{
+						Val: 3,
+					},
+					Right: nil,
+				},
+				Right: &TreeNode{
+					Val: 2,
+				},
+			},
+			2,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			ret := minDepth(c.input)
+			if !reflect.DeepEqual(ret, c.expect) {
+				t.Fatalf("expected: %v, but got: %v, with inputs: %v",
+					c.expect, ret, c.input)
+			}
+		})
+	}
+}
