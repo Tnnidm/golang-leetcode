bb9bf1a3169af9529f2d5b091a41f2c18268d6e6
tnnidm <910207973@qq.com>
2021-12-20 19:55:32 +0800

    complete 1356_SortIntegersbyTheNumberof1Bits

---
+++ 1301_1400/1356_SortIntegersbyTheNumberof1Bits/sortByBits.go
@@ -0,0 +1,19 @@
+package sortByBits
+
+import "sort"
+
+func sortByBits(a []int) []int {
+	sort.Slice(a, func(i, j int) bool {
+		x, y := a[i], a[j]
+		cx, cy := onesCount(x), onesCount(y)
+		return cx < cy || cx == cy && x < y
+	})
+	return a
+}
+
+func onesCount(x int) (c int) {
+	for ; x > 0; x /= 2 {
+		c += x % 2
+	}
+	return
+}
