10024d840b47d526a0441eed65bebd309bf3d9a8
tnnidm <910207973@qq.com>
2022-01-08 16:55:40 +0800

    1791_FindCenterofStarGraph

---
+++ 1701_1800/1791_FindCenterofStarGraph/findCenter.go
@@ -0,0 +1,9 @@
+package findCenter
+
+func findCenter(edges [][]int) int {
+	if edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] {
+		return edges[0][0]
+	} else {
+		return edges[0][1]
+	}
+}
