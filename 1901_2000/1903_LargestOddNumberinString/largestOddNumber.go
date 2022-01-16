a6208f93389f976d40045f04d2adc0e0ebdad1af
tnnidm <910207973@qq.com>
2022-01-16 17:08:24 +0800

    1903_LargestOddNumberinString

---
+++ 1901_2000/1903_LargestOddNumberinString/largestOddNumber.go
@@ -0,0 +1,10 @@
+package largestOddNumber
+
+func largestOddNumber(num string) string {
+	for i := len(num) - 1; i >= 0; i-- {
+		if (num[i]-'0')%2 == 1 {
+			return num[:i+1]
+		}
+	}
+	return ""
+}
