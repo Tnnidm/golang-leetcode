8b9464b55d330eb62e87fdab0f2c8e6cdbe21234
tnnidm <910207973@qq.com>
2022-02-22 20:44:02 +0800

    update 2 new methods

--- CodingInterview/15_hammingWeight/hammingWeight.go
+++ CodingInterview/15_hammingWeight/hammingWeight.go
@@ -9,14 +9,35 @@ package hammingWeight
 // 	return count
 // }
 
-// 使用和uint32(1)取或是否等于0也可以判断奇偶，复杂度比取余低
+// // 使用和uint32(1)取或是否等于0也可以判断奇偶，复杂度比取余低
+// func hammingWeight(num uint32) int {
+// 	count := 0
+// 	one := uint32(1)
+// 	for ; num != 0; num >>= 1 {
+// 		if one&num != 0 {
+// 			count++
+// 		}
+// 	}
+// 	return count
+// }
+
+// // 也可以反过来增加uint32(1)的位数
+// func hammingWeight(num uint32) int {
+// 	count := 0
+// 	one := uint32(1)
+// 	for ; one != 0; one <<= 1 {
+// 		if one&num != 0 {
+// 			count++
+// 		}
+// 	}
+// 	return count
+// }
+
+// 因为n和n-1做与操作后，n的最低位的1会变成0，那么在n整个变成0之前，能做几次这种操作就是有几个1
 func hammingWeight(num uint32) int {
 	count := 0
-	one := uint32(1)
-	for ; num != 0; num >>= 1 {
-		if one&num != 0 {
-			count++
-		}
+	for ; num != 0; num = num & (num - 1) {
+		count++
 	}
 	return count
 }
