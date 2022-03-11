b561daf5255cf2101f6604b65ce0fde3f0a942be
tnnidm <910207973@qq.com>
2022-03-11 20:21:08 +0800

    35_copyRandomList

--- CodingInterview/35_copyRandomList/copyRandomList.go
+++ CodingInterview/35_copyRandomList/copyRandomList.go
@@ -6,39 +6,48 @@ type Node struct {
 	Random *Node
 }
 
-// // assistNode应该可以不要
+// // 时间复杂度O(n)，空间复杂度O(n)
 // func copyRandomList(head *Node) *Node {
-// 	assistHead := &Node{}
-// 	assistHeadTemp := assistHead
-// 	listMap := map[*Node](*Node){nil: nil}
-// 	for temp := head; temp != nil; temp = temp.Next {
-// 		assistHeadTemp.Next = &Node{temp.Val, nil, nil}
-// 		listMap[temp] = assistHeadTemp.Next
-// 		assistHeadTemp = assistHeadTemp.Next
+// 	if head == nil {
+// 		return nil
 // 	}
-// 	for p1, p2 := head, assistHead.Next; p1 != nil; p1, p2 = p1.Next, p2.Next {
-// 		p2.Random = listMap[p1.Random]
+// 	count := 1
+// 	for p1 := head; p1 != nil; p1 = p1.Next {
+// 		count++
 // 	}
-// 	return assistHead.Next
+// 	newHead := &Node{head.Val, nil, nil}
+// 	addressMap := make(map[*Node](*Node), count)
+// 	addressMap[head] = newHead
+// 	for p1, p2 := head.Next, newHead; p1 != nil; p1, p2 = p1.Next, p2.Next {
+// 		p2.Next = &Node{p1.Val, nil, nil}
+// 		addressMap[p1] = p2.Next
+// 	}
+// 	for p1, p2 := head, newHead; p1 != nil; p1, p2 = p1.Next, p2.Next {
+// 		p2.Random = addressMap[p1.Random]
+// 	}
+// 	return newHead
 // }
 
+// 时间复杂度O(n)，空间复杂度O(1)
+// 但是题目不一定允许修改原始链表
 func copyRandomList(head *Node) *Node {
 	if head == nil {
 		return nil
 	}
-	count := 1
-	for p1 := head; p1 != nil; p1 = p1.Next {
-		count++
+	for p1 := head; p1 != nil; {
+		temp := p1.Next
+		p1.Next = &Node{p1.Val, temp, nil}
+		p1 = temp
 	}
-	newHead := &Node{head.Val, nil, nil}
-	addressMap := make(map[*Node](*Node), count)
-	addressMap[head] = newHead
-	for p1, p2 := head.Next, newHead; p1 != nil; p1, p2 = p1.Next, p2.Next {
-		p2.Next = &Node{p1.Val, nil, nil}
-		addressMap[p1] = p2.Next
+	for p1 := head; p1 != nil; p1 = p1.Next.Next {
+		if p1.Random != nil {
+			p1.Next.Random = p1.Random.Next
+		}
 	}
-	for p1, p2 := head, newHead; p1 != nil; p1, p2 = p1.Next, p2.Next {
-		p2.Random = addressMap[p1.Random]
+	for p1 := head.Next; p1 != nil; p1 = p1.Next {
+		temp := p1.Next
+		temp.Next = nil
+		p1.Next = p1.Next.Next
 	}
-	return newHead
+	return head.Next
 }
