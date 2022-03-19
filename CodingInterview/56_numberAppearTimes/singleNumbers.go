0ffa83290caf1e9442ba7c7b72d42ddbba95a48d
tnnidm <910207973@qq.com>
2022-03-19 16:09:16 +0800

    56-I_singleNumbers

---
+++ CodingInterview/56_numberAppearTimes/singleNumbers.go
@@ -0,0 +1,22 @@
+package numberAppearTimes
+
+func singleNumbers(nums []int) []int {
+	numsLen := len(nums)
+	temp := 0
+	for i := 0; i < numsLen; i++ {
+		temp ^= nums[i]
+	}
+	k := 1
+	for temp&k == 0 {
+		k <<= 1
+	}
+	ans := []int{0, 0}
+	for i := 0; i < numsLen; i++ {
+		if nums[i]&k == 0 {
+			ans[0] ^= nums[i]
+		} else {
+			ans[1] ^= nums[i]
+		}
+	}
+	return ans
+}
