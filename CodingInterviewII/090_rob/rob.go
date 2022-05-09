6e8f8cab1c9f82ef0139bf346b3906fcd1d21348
tnnidm <910207973@qq.com>
2022-05-09 16:26:36 +0800

    090_rob

--- CodingInterviewII/090_rob/rob.go
+++ CodingInterviewII/090_rob/rob.go
@@ -2,6 +2,13 @@ package rob
 
 func rob(nums []int) int {
 	numsLen := len(nums)
+	if numsLen == 1 {
+		return nums[0]
+	}
+	return max(robCore(nums[1:], numsLen-1), robCore(nums[:numsLen-1], numsLen-1))
+}
+
+func robCore(nums []int, numsLen int) int {
 	robNum := [2]int{nums[0], 0}
 	for i := 1; i < numsLen; i++ {
 		robNum[0], robNum[1] = robNum[1]+nums[i], max(robNum[0], robNum[1])
