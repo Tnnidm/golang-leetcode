876f9365876b3b2f6291b73528236f262935c947
tnnidm <910207973@qq.com>
2022-05-15 15:08:28 +0800

    098_uniquePaths

---
+++ CodingInterviewII/098_uniquePaths/uniquePaths.go
@@ -0,0 +1,18 @@
+package uniquePaths
+
+func uniquePaths(m int, n int) int {
+	if m < n {
+		return uniquePaths(n, m)
+	}
+	dp := make([]int, n)
+	for j := 0; j < n; j++ {
+		dp[j] = 1
+	}
+	for i := 1; i < m; i++ {
+		dp[0] = 1
+		for j := 1; j < n; j++ {
+			dp[j] += dp[j-1]
+		}
+	}
+	return dp[n-1]
+}
