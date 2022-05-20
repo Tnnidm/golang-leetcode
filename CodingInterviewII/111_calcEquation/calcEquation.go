4c7152b0da6ddebcc16b97e2a311b7ef5463e2aa
tnnidm <910207973@qq.com>
2022-05-20 17:14:53 +0800

    111_calcEquation

--- CodingInterviewII/111_calcEquation/calcEquation.go
+++ CodingInterviewII/111_calcEquation/calcEquation.go
@@ -1,5 +1,64 @@
 package calcEquation
 
+// // BFS
+// func calcEquation(equations [][]string, values []float64, queries [][]string) []float64 {
+// 	variable2Index := map[string]int{}
+// 	variableNum := 0
+// 	for _, equation := range equations {
+// 		if _, ok := variable2Index[equation[0]]; !ok {
+// 			variable2Index[equation[0]] = variableNum
+// 			variableNum++
+// 		}
+// 		if _, ok := variable2Index[equation[1]]; !ok {
+// 			variable2Index[equation[1]] = variableNum
+// 			variableNum++
+// 		}
+// 	}
+// 	calculationGraph := make([][]float64, variableNum)
+// 	for i := 0; i < variableNum; i++ {
+// 		calculationGraph[i] = make([]float64, variableNum)
+// 		calculationGraph[i][i] = 1.0
+// 	}
+// 	for i := 0; i < len(equations); i++ {
+// 		x := variable2Index[equations[i][0]]
+// 		y := variable2Index[equations[i][1]]
+// 		calculationGraph[x][y] = values[i]
+// 		calculationGraph[y][x] = 1 / values[i]
+// 	}
+// 	var dfs func(int, int) float64
+// 	var seen []bool
+// 	dfs = func(sourceIndex, targetIndex int) float64 {
+// 		if calculationGraph[sourceIndex][targetIndex] > 0 {
+// 			return calculationGraph[sourceIndex][targetIndex]
+// 		}
+// 		for i := 0; i < variableNum; i++ {
+// 			if i != sourceIndex && !seen[i] && calculationGraph[sourceIndex][i] > 0 {
+// 				seen[i] = true
+// 				temp := dfs(i, targetIndex)
+// 				seen[i] = false
+// 				if temp != -1.0 {
+// 					return calculationGraph[sourceIndex][i] * temp
+// 				}
+// 			}
+// 		}
+// 		return -1.0
+// 	}
+// 	queriesNum := len(queries)
+// 	result := make([]float64, queriesNum)
+// 	for i := 0; i < queriesNum; i++ {
+// 		sourceIndex, sourceExist := variable2Index[queries[i][0]]
+// 		targetIndex, targetExist := variable2Index[queries[i][1]]
+// 		if !sourceExist || !targetExist {
+// 			result[i] = -1.0
+// 		} else {
+// 			seen = make([]bool, variableNum)
+// 			result[i] = dfs(sourceIndex, targetIndex)
+// 		}
+// 	}
+// 	return result
+// }
+
+// Floyd算法
 func calcEquation(equations [][]string, values []float64, queries [][]string) []float64 {
 	variable2Index := map[string]int{}
 	variableNum := 0
@@ -13,6 +72,8 @@ func calcEquation(equations [][]string, values []float64, queries [][]string) []
 			variableNum++
 		}
 	}
+
+	// 建图
 	calculationGraph := make([][]float64, variableNum)
 	for i := 0; i < variableNum; i++ {
 		calculationGraph[i] = make([]float64, variableNum)
@@ -24,34 +85,27 @@ func calcEquation(equations [][]string, values []float64, queries [][]string) []
 		calculationGraph[x][y] = values[i]
 		calculationGraph[y][x] = 1 / values[i]
 	}
-	var dfs func(int, int) float64
-	var seen []bool
-	dfs = func(sourceIndex, targetIndex int) float64 {
-		if calculationGraph[sourceIndex][targetIndex] > 0 {
-			return calculationGraph[sourceIndex][targetIndex]
-		}
-		for i := 0; i < variableNum; i++ {
-			if i != sourceIndex && !seen[i] && calculationGraph[sourceIndex][i] > 0 {
-				seen[i] = true
-				temp := dfs(i, targetIndex)
-				seen[i] = false
-				if temp != -1.0 {
-					return calculationGraph[sourceIndex][i] * temp
+
+	// 执行 Floyd 算法
+	for k := range calculationGraph {
+		for i := range calculationGraph {
+			for j := range calculationGraph {
+				if calculationGraph[i][k] > 0 && calculationGraph[k][j] > 0 {
+					calculationGraph[i][j] = calculationGraph[i][k] * calculationGraph[k][j]
 				}
 			}
 		}
-		return -1.0
 	}
+
 	queriesNum := len(queries)
 	result := make([]float64, queriesNum)
 	for i := 0; i < queriesNum; i++ {
 		sourceIndex, sourceExist := variable2Index[queries[i][0]]
 		targetIndex, targetExist := variable2Index[queries[i][1]]
-		if !sourceExist || !targetExist {
+		if !sourceExist || !targetExist || calculationGraph[sourceIndex][targetIndex] == 0 {
 			result[i] = -1.0
 		} else {
-			seen = make([]bool, variableNum)
-			result[i] = dfs(sourceIndex, targetIndex)
+			result[i] = calculationGraph[sourceIndex][targetIndex]
 		}
 	}
 	return result
