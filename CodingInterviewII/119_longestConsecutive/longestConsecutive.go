8062e1f0967716887461539885e2076dad7703f9
tnnidm <910207973@qq.com>
2022-05-22 19:48:57 +0800

    119_longestConsecutive

---
+++ CodingInterviewII/119_longestConsecutive/longestConsecutive.go
@@ -0,0 +1,24 @@
+package longestConsecutive
+
+// HASH表做法
+func longestConsecutive(nums []int) int {
+	numSet := map[int]bool{}
+	for _, num := range nums {
+		numSet[num] = true
+	}
+	longestStreak := 0
+	for num := range numSet {
+		if !numSet[num-1] {
+			currentNum := num
+			currentStreak := 1
+			for numSet[currentNum+1] {
+				currentNum++
+				currentStreak++
+			}
+			if longestStreak < currentStreak {
+				longestStreak = currentStreak
+			}
+		}
+	}
+	return longestStreak
+}
